d424c133a07e03e91e72f7d25b2f989d3f627a64
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-22 12:19:51 +0200

    ✨Creación de tabla con datos de placeholder

--- src/app/components/listado-mascotas/listado-mascotas.component.ts
+++ src/app/components/listado-mascotas/listado-mascotas.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Mascota } from 'src/app/interfaces/mascota';
+
+const ELEMENT_DATA: Mascota[] = [
+  { nombre: 'Pancho', color: 'Amarillo', edad: 10, raza: 'Mestizo', peso: 10 },
+  { nombre: 'Cuca', color: 'Blanco', edad: 8, raza: 'Bodeguero', peso: 8 },
+  { nombre: 'Luna', color: 'Negro', edad: 7, raza: 'Labrador', peso: 30 },
+  { nombre: 'Bella', color: 'Blanco', edad: 2, raza: 'Husky', peso: 20 },
+  { nombre: 'Max', color: 'Marrón', edad: 5, raza: 'Boxer', peso: 25 },
+  { nombre: 'Rocky', color: 'Gris', edad: 4, raza: 'Bulldog', peso: 24 },
+  
+];
+
 
 @Component({
   selector: 'app-listado-mascotas',
@@ -6,8 +19,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado-mascotas.component.css']
 })
 export class ListadoMascotasComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['nombre','edad','raza','color','peso'];
+  dataSource = ELEMENT_DATA;
 
   ngOnInit(): void {
   }
