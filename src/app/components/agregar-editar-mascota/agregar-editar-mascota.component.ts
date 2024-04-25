e332e0f74e44049b2f7a59c47e294b71f0bdd6a3
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-25 10:01:59 +0200

    ✨Agregar mascota

--- src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
+++ src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { Mascota } from 'src/app/interfaces/mascota';
+import { MascotaService } from 'src/app/services/mascota.service';
 
 @Component({
   selector: 'app-agregar-editar-mascota',
@@ -8,25 +11,28 @@ import { Mascota } from 'src/app/interfaces/mascota';
   styleUrls: ['./agregar-editar-mascota.component.css']
 })
 export class AgregarEditarMascotaComponent implements OnInit {
-  loading : boolean = false;
+  loading: boolean = false;
   form: FormGroup;
 
-  constructor(private fb : FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private _mascotaService: MascotaService,
+    private _snackBar: MatSnackBar,
+    private router: Router) {
     this.form = this.fb.group({
-      nombre : ['', Validators.required],
-      raza : ['', Validators.required],
-      color : ['', Validators.required],
-      edad : ['', Validators.required],
-      peso : ['', Validators.required],
+      nombre: ['', Validators.required],
+      raza: ['', Validators.required],
+      color: ['', Validators.required],
+      edad: ['', Validators.required],
+      peso: ['', Validators.required],
     })
-   }
+  }
 
   ngOnInit(): void {
   }
 
-  agregarMascota()
-  {
-    const mascota : Mascota = {
+  agregarMascota() {
+    // Armamos objeto
+    const mascota: Mascota = {
       nombre: this.form.value.nombre,
       edad: this.form.value.edad,
       raza: this.form.value.raza,
@@ -34,7 +40,20 @@ export class AgregarEditarMascotaComponent implements OnInit {
       peso: this.form.value.peso
     }
 
-    console.log(mascota);
+    // Enviamos objeto al back-end
+    this._mascotaService.addMascota(mascota).subscribe(data => {
+      console.log(data);
+      this.mensaje_exito();
+      this.router.navigate(['/listMascotas']);
+    }
+    )
+  }
+
+  mensaje_exito() {
+    this._snackBar.open("La mascota fue registrada con éxito", '', {
+      duration: 4000,
+      horizontalPosition: 'left',
+    });
   }
 
 }
